55d8f2fd3d798906669bf99043872ca19826a9d5
Jomana <jomanajihad6@gmail.com>
2021-05-07 20:22:31 +0300

    Add database code to Sign Up / Booking Details

--- app/src/main/java/com/example/graduationproject/MainActivity.java
+++ app/src/main/java/com/example/graduationproject/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.graduationproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.content.ContentValues.TAG;
 
 public class MainActivity extends AppCompatActivity {
     FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -19,8 +31,7 @@ public class MainActivity extends AppCompatActivity {
         final Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, vendorvenuespage.class));
-
+                startActivity(new Intent(MainActivity.this, bookingdetails.class));
             }
             });
             }
--- app/src/main/java/com/example/graduationproject/bookingdetails.java
+++ app/src/main/java/com/example/graduationproject/bookingdetails.java
@@ -1,14 +1,67 @@
 package com.example.graduationproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class bookingdetails extends AppCompatActivity {
+    public static final String BookingDate_Key="date";
+    public static final String CardName_Key="nameCard";
+    public static final String CardNumber_Key="cardNumber";
+    public static final String ExpireDate_Key="expireDate";
+    public static final String cvc_Key="cvc";
+    public static final String Tag="BookingDetail";
 
+    private DocumentReference mDocRef = FirebaseFirestore.getInstance().document("bookings/bookingSample");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bookingdetails);
+
+    }
+    public void saveBooking (View v) {
+        TextInputLayout BookingDate = (TextInputLayout) findViewById(R.id.customerBookingDate);
+        TextInputLayout NameCard = (TextInputLayout) findViewById(R.id.nameOnCard);
+        TextInputLayout NumberOfCard = (TextInputLayout) findViewById(R.id.cardNumber);
+        TextInputLayout ExpireDate = (TextInputLayout) findViewById(R.id.cardExpireDate);
+        TextInputLayout CVC = (TextInputLayout) findViewById(R.id.cardCVC);
+
+        Map<String,Object> booking = new HashMap<>();
+        booking.put(BookingDate_Key,BookingDate);
+        booking.put(CardName_Key,NameCard);
+        booking.put(CardNumber_Key, NumberOfCard);
+        booking.put(ExpireDate_Key, ExpireDate);
+        booking.put(cvc_Key, CVC);
+        mDocRef.set(booking).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d(Tag,"Document has been added!");
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.w(Tag,"Document was not saved!",e);
+
+            }
+        });
+
+
     }
 }
