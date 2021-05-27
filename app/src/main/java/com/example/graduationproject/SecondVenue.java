4ae858a875cfd45b0e5e905b3f5144b806616c15
MarahBarakat <mbarakat@asaltech.com>
2021-05-27 16:07:52 +0300

    Marah Updates in this Activity: vendorhomepage vendorcreatevenue SingleVenue vendormultiplechat vendorchat

--- app/src/main/java/com/example/graduationproject/SecondVenue.java
+++ app/src/main/java/com/example/graduationproject/SecondVenue.java
@@ -8,20 +8,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-public class SingleVenue extends AppCompatActivity {
+public class SecondVenue extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_single_venue);
-
+        setContentView(R.layout.activity_second_venue);
         TextView maptext = (TextView) findViewById(R.id.textView15);
         maptext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent mapsIntent =new Intent();
                 mapsIntent.setAction(Intent.ACTION_VIEW);
-                mapsIntent.setData(Uri.parse("geo:31.888939,35.2005626"));
+                mapsIntent.setData(Uri.parse("geo:31.9028846,35.222873"));
                 startActivity(mapsIntent);
             }
         });
--- app/src/main/java/com/example/graduationproject/vendorhomepage.java
+++ app/src/main/java/com/example/graduationproject/vendorhomepage.java
@@ -2,7 +2,11 @@ package com.example.graduationproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class vendorhomepage extends AppCompatActivity {
 
@@ -10,5 +14,30 @@ public class vendorhomepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vendorhomepage);
+
+        Button button = (Button) findViewById(R.id.button);
+        ImageView imageView1= (ImageView) findViewById(R.id.imageView131);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent1 = new
+                        Intent(vendorhomepage.this,vendorcreatevenue.class);
+                vendorhomepage.this.startActivity(intent1);
+                finish();
+
+            }
+
+        });
+        imageView1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent1 = new
+                        Intent(vendorhomepage.this,SingleVenue.class);
+                vendorhomepage.this.startActivity(intent1);
+                finish();
+
+            }
+
+        });
+
+
     }
 }
